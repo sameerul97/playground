59d8ba860df4f13e6e46b68e63c01a15eab9efcb
sameerul97 <sameerul97@gmail.com>
2024-11-29 22:59:17 +0000

    add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image"
-import { getFullPath } from "@/helpers/pathHelper"
-
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <Image
-            className="dark:invert"
-            src={getFullPath("/next.svg")}
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <h1 className="font-bold">Playground</h1>
-        </div>
-      </main>
-    </div>
+    <main className="w-full">
+      <div className="flex size-full flex-1 flex-col justify-center gap-4 p-4 align-middle">
+        <h1 className="text-center font-bold">Playground</h1>
+      </div>
+    </main>
   )
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,43 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Camera Look At",
+    url: "/playground/camera-look-at",
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant="floating">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Demos</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
