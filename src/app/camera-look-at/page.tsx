39d81d6ae993fa22c2153ca1968dc2d5445141e7
sameerul97 <sameerul97@gmail.com>
2024-11-29 22:59:47 +0000

    add camera look at page

---
+++ src/app/camera-look-at/page.tsx
@@ -0,0 +1,9 @@
+export default function CameraLookAt() {
+  return (
+    <main className="w-full">
+      <div className="flex size-full flex-1 flex-col justify-center gap-4 p-4 align-middle">
+        <h1 className="text-center font-bold">Camera Look At</h1>
+      </div>
+    </main>
+  )
+}
