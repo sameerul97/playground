43d4e9868728cae8b585f99daadd535c32c9ece8
sameerul97 <sameerul97@gmail.com>
2025-01-25 16:22:36 +0000

    refactor + add typing

--- src/app/sticker-ball/page.tsx
+++ src/app/sticker-ball/page.tsx
@@ -9,7 +9,7 @@ import {
   OrbitControls,
   useTexture,
 } from "@react-three/drei"
-import { Canvas, useFrame } from "@react-three/fiber"
+import { Canvas, ThreeEvent, useFrame } from "@react-three/fiber"
 import { Leva, useControls } from "leva"
 import * as THREE from "three"
 
@@ -25,15 +25,61 @@ const STATION_IMAGES = [
 const DEFAULT_SCALE = [0.5, -0.25, 0.25]
 const THRESHOLD = 10
 
-const Stickers = () => {
+interface StationLogo {
+  position: THREE.Vector3
+  rotation: number
+  scale: THREE.Vector3
+  texture: THREE.Texture
+  renderOrder: number
+}
+
+const RayoBallSticker = () => {
   const [initialMousePosition, setInitialMousePosition] = useState({
     x: 0,
     y: 0,
   })
+  const [targetScale, setTargetScale] = useState<THREE.Vector3>(
+    new THREE.Vector3(1, 1, 1)
+  )
+  const [stationLogos, setStationLogos] = useState<StationLogo[]>([])
 
-  const materialRef = useRef(null)
+  const textures = useTexture(STATION_IMAGES)
+  const rayo = useTexture(getFullPath("/stations/rayo.png"))
+
+  const rayoBallMesh = useRef<THREE.Mesh>(null)
+  const materialRef = useRef<THREE.MeshStandardMaterial>(null)
   const wobbleFactorRef = useRef(1)
   const wobbleSpeedRef = useRef(1)
+
+  const {
+    color,
+    metalness,
+    roughness,
+    emissive,
+    animateScale,
+    // factor,
+    // speed,
+  } = useControls("Rayo Ball Props", {
+    color: "#fff",
+    emissive: "#000",
+    metalness: { value: 0.15, min: 0, max: 2 },
+    roughness: { value: 0.26, min: 0, max: 1 },
+    animateScale: { value: true },
+    // factor: { value: 1, min: 0, max: 20 },
+    // speed: { value: 10, min: 0, max: 50 },
+  })
+
+  useFrame((_, delta) => {
+    if (animateScale) {
+      const smoothing = 8 // Adjust for smoother/faster transitions
+      const mesh = rayoBallMesh.current
+
+      if (mesh) {
+        // Interpolate the scale of the mesh directly
+        mesh.scale.lerp(targetScale, smoothing * delta)
+      }
+    }
+  })
 
   useFrame(() => {
     if (materialRef.current) {
@@ -48,54 +94,20 @@ const Stickers = () => {
     }
   })
 
-  const [stationLogos, setStationLogos] = useState<THREE.Texture[]>([])
-  const textures = useTexture(STATION_IMAGES)
-  const rayo = useTexture(getFullPath("/stations/rayo.png"))
-  const {
-    color,
-    metalness,
-    roughness,
-    emissive,
-    // factor,
-    // speed,
-  } = useControls("Rayo Ball Props", {
-    color: "#fff",
-    emissive: "#000",
-    metalness: { value: 0.15, min: 0, max: 2 },
-    roughness: { value: 0.26, min: 0, max: 1 },
-
-    // factor: { value: 1, min: 0, max: 20 },
-    // speed: { value: 10, min: 0, max: 50 },
-  })
-
-  const {
-    stickerColor,
-    stickerMetalness,
-    stickerRoughness,
-    stickerClearcoat,
-    stickerClearcoatRoughness,
-  } = useControls("Sticker Props", {
-    stickerColor: "#fff",
-    stickerMetalness: { value: 2, min: 0, max: 4 },
-    stickerRoughness: { value: 1, min: 0, max: 1 },
-    stickerClearcoat: { value: 0.6, min: 0, max: 1 },
-    stickerClearcoatRoughness: { value: 0.5, min: 0, max: 1 },
-  })
-
   useEffect(() => {
     // rayo.flipY = false
     // rayo.repeat.set(2, 1)
+    // rayo.WrapT = THREE.RepeatWrapping
     rayo.offset.set(0.2, 0)
     rayo.wrapS = THREE.RepeatWrapping
-    // rayo.WrapT = THREE.RepeatWrapping
     rayo.needsUpdate = true
   }, [rayo])
 
-  useEffect(() => {
-    // textures.forEach((texture) => {
-    //   texture.needsUpdate = true
-    // })
-  }, [textures])
+  // useEffect(() => {
+  // textures.forEach((texture) => {
+  //   texture.needsUpdate = true
+  // })
+  // }, [textures])
 
   const handleOnMouseDown = useCallback(
     (event: { clientX: never; clientY: never }) => {
@@ -105,7 +117,7 @@ const Stickers = () => {
   )
 
   const handleOnMouseUp = useCallback(
-    (event: { clientX: number; clientY: number; point: never }) => {
+    (event: ThreeEvent<PointerEvent>) => {
       if (
         !(
           Math.abs(event.clientX - initialMousePosition.x) > THRESHOLD ||
@@ -115,10 +127,13 @@ const Stickers = () => {
         setStationLogos([
           ...stationLogos,
           {
-            // @ts-expect-error temp ignore
             position: event.point,
             rotation: Math.PI * (Math.random() * (2.2 - 1.8) + 1.8),
-            scale: DEFAULT_SCALE.map((s) => s + Math.random() * 0.1),
+            scale: new THREE.Vector3(
+              DEFAULT_SCALE[0] + Math.random() * 0.1,
+              DEFAULT_SCALE[1] + Math.random() * 0.1,
+              DEFAULT_SCALE[2] + Math.random() * 0.1
+            ),
             texture: textures[Math.floor(Math.random() * textures.length)],
             renderOrder: stationLogos.length,
           },
@@ -126,59 +141,112 @@ const Stickers = () => {
 
         wobbleFactorRef.current = wobbleFactorRef.current === 1 ? 2 : 1
         wobbleSpeedRef.current = wobbleSpeedRef.current === 1 ? 1.5 : 1
+
+        if (animateScale) {
+          const randomScale = new THREE.Vector3(
+            1 + Math.random() * 0.5,
+            1 + Math.random() * 0.3,
+            1 + Math.random() * 0.4
+          )
+          setTargetScale(randomScale)
+
+          const randomDelay = Math.random() * (300 - 100) + 100
+
+          setTimeout(() => {
+            setTargetScale(new THREE.Vector3(1, 1, 1))
+          }, randomDelay)
+        }
       }
     },
-    [stationLogos, initialMousePosition, textures]
+    [animateScale, stationLogos, initialMousePosition, textures]
   )
+
+  return (
+    <mesh
+      ref={rayoBallMesh}
+      castShadow
+      receiveShadow
+      onPointerDown={handleOnMouseDown}
+      onPointerUp={handleOnMouseUp}
+    >
+      <sphereGeometry args={[1, 64, 64]} />
+      {/* <DecalPhysicalMaterialSticker stationLogos={stationLogos} /> */}
+      {stationLogos.map((sticker, i) => (
+        <Decal key={i} {...sticker}>
+          <meshBasicMaterial
+            map={sticker.texture}
+            polygonOffsetFactor={-10}
+            transparent={true}
+            polygonOffset={true}
+            map-flipY={false}
+            depthWrite={false}
+            toneMapped={false}
+          />
+        </Decal>
+      ))}
+
+      <MeshWobbleMaterial
+        ref={materialRef}
+        roughness={roughness}
+        metalness={metalness}
+        color={color}
+        emissive={emissive}
+        map={rayo}
+        factor={1}
+        speed={1}
+        depthWrite={false}
+      />
+      {/* <PhysicalMaterialBall texture={rayo} /> */}
+    </mesh>
+  )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const DecalPhysicalMaterialSticker = ({
+  stationLogos,
+}: {
+  stationLogos: StationLogo[]
+}) => {
+  const {
+    color,
+    metalness,
+    roughness,
+    clearcoat,
+    clearcoatRoughness,
+    transmission,
+    thickness,
+  } = useControls("Physical Material Sticker Props", {
+    color: "#fff",
+    metalness: { value: 2, min: 0, max: 4 },
+    roughness: { value: 1, min: 0, max: 2 },
+    clearcoat: { value: 0.6, min: 0, max: 2 },
+    clearcoatRoughness: { value: 0.5, min: 0, max: 2 },
+    transmission: { value: 0.6, min: 0, max: 2 },
+    thickness: { value: 0.6, min: 0, max: 2 },
+  })
+
   return (
     <>
-      <mesh
-        castShadow
-        receiveShadow
-        onPointerDown={handleOnMouseDown}
-        onPointerUp={handleOnMouseUp}
-      >
-        <sphereGeometry args={[1, 64, 64]} />
-        {stationLogos.map((sticker, i) => (
-          <Decal key={i} {...sticker}>
-            <meshPhysicalMaterial
-              // @ts-expect-error temp ignore
-              map={sticker.texture}
-              //   roughness={1}
-              //   metalness={1}
-              //   clearcoat={0.1}
-              //   clearcoatRoughness={1}
-              transmission={1}
-              thickness={1}
-              color={stickerColor}
-              roughness={stickerMetalness}
-              metalness={stickerRoughness}
-              clearcoat={stickerClearcoat}
-              clearcoatRoughness={stickerClearcoatRoughness}
-              polygonOffsetFactor={-10}
-              transparent={true}
-              polygonOffset={true}
-              map-flipY={false}
-              depthWrite={false}
-              toneMapped={false}
-            />
-          </Decal>
-        ))}
-        <MeshWobbleMaterial
-          ref={materialRef}
-          roughness={roughness}
-          metalness={metalness}
-          color={color}
-          emissive={emissive}
-          map={rayo}
-          factor={1}
-          speed={1}
-          // factor={factor}
-          // speed={speed}
-          depthWrite={false}
-        />
-        {/* <PhysicalMaterialBall texture={rayo} /> */}
-      </mesh>
+      {stationLogos.map((sticker, i) => (
+        <Decal key={i} {...sticker}>
+          <meshPhysicalMaterial
+            map={sticker.texture}
+            transmission={transmission}
+            thickness={thickness}
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+            clearcoat={clearcoat}
+            clearcoatRoughness={clearcoatRoughness}
+            polygonOffsetFactor={-10}
+            transparent={true}
+            polygonOffset={true}
+            map-flipY={false}
+            depthWrite={false}
+            toneMapped={false}
+          />
+        </Decal>
+      ))}
     </>
   )
 }
@@ -197,7 +265,6 @@ const PhysicalMaterialBall = ({ texture }: { texture: THREE.Texture }) => {
     color: "#fff",
     metalness: { value: 0.15, min: 0, max: 2 },
     roughness: { value: 0.26, min: 0, max: 1 },
-
     clearcoat: { value: 0.6, min: 0, max: 1 },
     clearcoatRoughness: { value: 0.5, min: 0, max: 1 },
     transmission: { value: 0.6, min: 0, max: 1 },
@@ -206,9 +273,6 @@ const PhysicalMaterialBall = ({ texture }: { texture: THREE.Texture }) => {
 
   return (
     <meshPhysicalMaterial
-      // color="black"
-      // metalness={0.5}
-      // roughness={1}
       clearcoat={clearcoat}
       clearcoatRoughness={clearcoatRoughness}
       transmission={transmission}
@@ -222,12 +286,20 @@ const PhysicalMaterialBall = ({ texture }: { texture: THREE.Texture }) => {
 }
 useTexture.preload(STATION_IMAGES)
 
+const HemisphereLight = () => {
+  const { ...hemLightProps } = useControls("HemisphereLight Props", {
+    color: "#fff",
+    groundColor: "#fff",
+    intensity: { value: 1, min: 1, max: 4 },
+  })
+
+  return <hemisphereLight {...hemLightProps} />
+}
 export default function App() {
   return (
     <main className="purple-bg w-full overflow-x-auto">
       <Leva />
       <Canvas shadows camera={{ position: [0, 0.5, 5], fov: 45 }}>
-        {/* <color attach="background" args={["#fff"]} /> */}
         <OrbitControls
           enableDamping
           maxPolarAngle={1.5}
@@ -236,9 +308,8 @@ export default function App() {
           maxDistance={6}
         />
         <Environment preset="warehouse" environmentIntensity={1} />
-        <hemisphereLight intensity={1} />
-
-        <Stickers />
+        <HemisphereLight />
+        <RayoBallSticker />
       </Canvas>
     </main>
   )
